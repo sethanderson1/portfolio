9867e2bff86e00207b8ec51c9b63a5de2924c44d
Seth Anderson <sdande02@gmail.com>
2020-01-26 16:28:50 -0700

    greybox complete

---
+++ index.js
@@ -0,0 +1,47 @@
+
+// $('.scroller').on('click',function(event) {
+//        // need to account for nav bar so need to subtract height of navbar or something
+//     // const height = $('nav').height();
+//     // console.log(height)
+//     $('html,body').animate({
+//         scrollTop: $( $(this).attr('href')).offset().top
+//     },600)
+// })
+
+
+
+
+// function displayResults(responseJson,maxResults) {
+//     $('#results-list').empty();
+
+//     for (let i = 0; i< responseJson.length && i < maxResults; i++) {
+//         $('#results-list').append(
+//             `<li><a href="${responseJson[i].html_url}">
+//             ${responseJson[i].name}</a></li>`
+//         )
+//     }
+
+//     $('#results-list').removeClass('hidden');
+
+// }
+
+// function handlePage() {
+
+// }
+
+// $(handlePage)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
