50e5875e6a8bbe47cb068c4289dcccdcd5a6fcea
Seth Anderson <sdande02@gmail.com>
2020-02-25 02:05:41 -0700

    change link hover color

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
-// $('.scroller').on('click',function(event) {
-//        // need to account for nav bar so need to subtract height of navbar or something
-//     // const height = $('nav').height();
-//     // console.log(height)
-//     $('html,body').animate({
-//         scrollTop: $( $(this).attr('href')).offset().top
-//     },600)
-// })
+$('.scroller').on('click',function(event) {
+       // need to account for nav bar so need to subtract height of navbar or something
+    // const height = $('nav').height();
+    // console.log(height)
+    $('html,body').animate({
+        scrollTop: $( $(this).attr('href')).offset().top
+    },600)
+})
 
 
 
