6aead2820cc7cce4ea342523ac88d2aff0d721ef
Seth Anderson <sdande02@gmail.com>
2020-03-08 13:02:17 -0700

    deleted commented code

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 
 $('.scroller').on('click', function (event) {
-    // need to account for nav bar so need to subtract height of navbar or something
-    // const height = $('nav').height();
-    // console.log(height)
     $('html,body').animate({
         scrollTop: $($(this).attr('href')).offset().top
     }, 600)
@@ -21,38 +18,3 @@ $('.scroller').on('click', function (event) {
 //         $("header .icons-media").css('bottom', bottomPos).css('position', 'fixed');
 //     }
 // });
-
-// function displayResults(responseJson,maxResults) {
-//     $('#results-list').empty();
-
-//     for (let i = 0; i< responseJson.length && i < maxResults; i++) {
-//         $('#results-list').append(
-//             `<li><a href="${responseJson[i].html_url}">
-//             ${responseJson[i].name}</a></li>`
-//         )
-//     }
-
-//     $('#results-list').removeClass('hidden');
-
-// }
-
-// function handlePage() {
-
-// }
-
-// $(handlePage)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
