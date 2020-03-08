634888df84eafe45f955b36a6f161ececf22df5a
Seth Anderson <sdande02@gmail.com>
2020-03-08 02:26:46 -0700

    test 5 scroll icons header

--- index.js
+++ index.js
@@ -9,18 +9,18 @@ $('.scroller').on('click', function (event) {
 })
 
 
-// $(window).scroll(function () {
-//     const scrollThreshold = 56;
-//     const bottomPos = 56;
-//     console.log('$(window).scrollTop()', $(window).scrollTop())
-//     // console.log('$(window).height()',$(window).height())
-//     if ($(window).scrollTop() >= scrollThreshold) {  // change target to number
-//         $(".icons-media").css('bottom', 0).css('position', 'absolute');
-//     }
-//     if ($(window).scrollTop() < scrollThreshold) {  // change target to number
-//         $(".icons-media").css('bottom', -56).css('position', 'fixed');
-//     }
-// });
+$(window).scroll(function () {
+    const scrollThreshold = 56;
+    const bottomPos = 56;
+    console.log('$(window).scrollTop()', $(window).scrollTop())
+    // console.log('$(window).height()',$(window).height())
+    if ($(window).scrollTop() >= scrollThreshold) {  // change target to number
+        $(".icons-media").css('bottom', 0).css('position', 'absolute');
+    }
+    if ($(window).scrollTop() < scrollThreshold) {  // change target to number
+        $(".icons-media").css('bottom', bottomPos).css('position', 'fixed');
+    }
+});
 
 // function displayResults(responseJson,maxResults) {
 //     $('#results-list').empty();
