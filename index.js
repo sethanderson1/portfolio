ea6e36bc40e6427078429608091218dc699da9fc
Seth Anderson <sdande02@gmail.com>
2020-03-08 02:03:47 -0700

    test scroll 3

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ $(window).scroll(function () {
     console.log('$(window).scrollTop()', $(window).scrollTop())
     // console.log('$(window).height()',$(window).height())
     if ($(window).scrollTop() >= scrollThreshold) {  // change target to number
-        $(".icons-media").css('bottom', 0).css('position', 'absolute');
+        $(".icons-media").css('bottom', -56).css('position', 'absolute');
     }
     if ($(window).scrollTop() < scrollThreshold) {  // change target to number
-        $(".icons-media").css('bottom', bottomPos).css('position', 'fixed');
+        $(".icons-media").css('bottom', 0).css('position', 'fixed');
     }
 });
 
